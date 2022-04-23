9d9499f2c19fca27953e0f2eb5952c305432aa74
facu-conci <facui.conci97@gmail.com>
2022-04-23 00:55:07 -0300

    compiling and linking the library

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
-g++ -I ./headers  ./sources/main.cpp ./sources/Menu.cpp ./sources/__utils.cpp -o menu_test
+::g++ -I ./headers  ./sources/main.cpp ./sources/Menu.cpp ./sources/__utils.cpp -o menu_test
+::cls
+::menu_test
 
-cls
 
-menu_test
+cd build
+
+cmake .. -G "MinGW Makefiles"
+
+mingw32-make
